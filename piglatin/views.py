127df80974e7aeb98d8b4111fab6032ac9875571
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-27 13:27:29 +1100

    yeah, took the docs too literally, the answer was much, much shorter, i also was attemtpting to direct the render function to the rroot directory without thinking that it already defaults to root due to us changing the settings file in the previous commit to add the templates folder to our DIRS env var, so yeah, just have to put the html file name, pretty simple

--- piglatin/views.py
+++ piglatin/views.py
@@ -2,6 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def helloworld(request):
-    return render(request, 'piglatin-py/piglatin_input.html', {
-        'foo': 'bar',
-    }, content_type='application/xhtml+xml')
+    return render(request, 'piglatin_input.html')
